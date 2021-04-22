bd7a123dec5fc4afbdf47e9e38969a80883c64b0
黄炎 <yan.huang@skyviewfund.com>
2021-04-22 15:55:44 +0800

    feat(hy):var schma

--- mongoengine/condition_validator.py
+++ mongoengine/condition_validator.py
@@ -2,20 +2,21 @@ from mongoengine import errors, BooleanField, EmbeddedDocumentField, EmbeddedDoc
 import json
 from collections import defaultdict
 from importlib import import_module, _bootstrap_external
+from .document import get_field, copy_field
 
 
-def deepcopy(cls_copy):
+def deepcopy(cls):
     """
         用重载module的方式 对cls进行深拷贝
-    :param cls_copy:
+    :param cls:
     :return:
     """
-    test_module = import_module(cls_copy.__module__)
+    test_module = import_module(cls.__module__)
     module_path, _, suffix = test_module.__file__.rpartition('.')
     module_path, _, module_name = module_path.rpartition('/')
     ff = _bootstrap_external.FileFinder(module_path, (_bootstrap_external.SourceFileLoader, ['.py']))
     mod = ff.find_module(module_name).load_module()
-    return getattr(mod, cls_copy.__name__)
+    return getattr(mod, cls.__name__)
 
 
 class ValidationErrorEncoder(json.JSONEncoder):
@@ -32,10 +33,41 @@ class ConditionValidatorMixin:
         依据不同的规则（用户权限、用户与数据的关系），进行不同的校验，或提供不同的Schema
         权限和条件类可变逻辑难于记忆，故强制要求在代码里写明判定逻辑，并在schema接口返回描述到前端。
     """
-    rules_desc = {
-
-    }
+    rules_desc = {}
     validate_conf = {}
+
+    def validate_according_to1(self, action):
+        ori_cls = type(self)
+        fields = ori_cls.validate_conf[action].get('check_fields', [])
+        res = defaultdict(dict)
+        for f in fields:
+            key = f
+            if isinstance(f, (list, tuple)):
+                key = f[0]
+                res[key] = dict(f[1])
+            res[key].update({'required': True})
+        fields = res
+        cls_fields_dict = {}
+        # 确认校验替换参数 String型加入了默认参数
+        for field_key in fields:
+            field = get_field(ori_cls, field_key)
+            cls_fields_dict[field_key] = field
+            if type(field) is StringField and not field.min_length and 'min_length' not in fields[field_key]:
+                fields[field_key].update({'min_length': 1})
+        attrs = {}
+        for field_key in fields:
+            if '.' not in field_key:
+                attrs[field_key] = copy_field(get_field(ori_cls, field_key), min_length=5)
+        cls_copy = ori_cls.copy_base_class( **attrs)
+
+        try:
+            self_copy = cls_copy.create_with(self.to_dict())
+            self_copy.validate()
+        except errors.ValidationError as e:
+            result = json.loads(json.dumps(e.errors, cls=ValidationErrorEncoder))
+        except Exception as e:
+            raise e
+        return result
 
     def validate_according_to(self, action):
         """
@@ -58,29 +90,11 @@ class ConditionValidatorMixin:
             res[key].update({'required': True})
         fields = res
 
-        def _get_field(cls_copy, key):
-            if '.' not in key:
-                field = cls_copy._fields[key]
-                return field
-            else:
-                keys = key.split('.', 1)
-                attr_key = keys[0]
-                new_key = keys[1]
-                v = cls_copy._fields.get(attr_key)
-                if new_key == '_str_':
-                    return v.field
-                if type(v) in (EmbeddedDocumentField, EmbeddedDocumentListField):
-                    res = _get_field(v.document_type, new_key)
-                else:
-                    # 目前支持的复杂field 仅存在 ListField 及以上两种。
-                    res = _get_field(v.field.document_type, new_key)
-                return res
-
         cls_copy = deepcopy(ori_cls)
         cls_fields_dict = {}
         # 确认校验替换参数 String型加入了默认参数
         for field_key in fields:
-            field = _get_field(cls_copy, field_key)
+            field = get_field(cls_copy, field_key)
             cls_fields_dict[field_key] = field
             if type(field) is StringField and not field.min_length and 'min_length' not in fields[field_key]:
                 fields[field_key].update({'min_length': 1})
--- mongoengine/document.py
+++ mongoengine/document.py
@@ -1,16 +1,63 @@
+import uuid
+from copy import deepcopy, copy
 from bson import ObjectId
-from copy import deepcopy
-from mongoengine import fields, EmbeddedDocumentField, ReferenceField, DateTimeField, DateField, ListField
+from mongoengine import fields, EmbeddedDocumentField, ReferenceField, DateTimeField, DateField, ListField, Document, EmbeddedDocument
+from mongoengine.base.metaclasses import DocumentMetaclass, TopLevelDocumentMetaclass
 try:
     from dateutil import parser
 except:
     from dateparser import parser
 from ..widgets.decorators import classproperty
-from ..config import CURRENT_REST_PLUS_CONFIG
+
+
+def get_field_cls(cls, key, silent=True):
+    def _get_cls(v):
+        if hasattr(v, 'document_type'):
+            return v.document_type
+        elif hasattr(v, 'field') and hasattr(v.field, 'document_type'):
+            return v.field.document_type
+        else:
+            if silent:
+                return None
+            raise Exception('not a document_type')
+    return _get_cls(get_field(cls, key))
+
+
+def get_field(cls, key):
+    if '.' not in key:
+        field = cls._fields[key]
+        return field
+    attr_key, new_key = key.split('.', 1)
+    v = cls._fields.get(attr_key)
+    if hasattr(v, 'document_type'):
+        return get_field(v.document_type, new_key)
+    else:
+        return get_field(v.field.document_type, new_key)
+
+
+def copy_field(_field, **kwargs):
+    new_field = copy(_field)
+    new_field.__dict__.update(kwargs)
+    return new_field
+
+
+def copy_cls(cls, base_cls=None, **kwargs):
+    if not base_cls:
+        base_cls = cls.__bases__[0]
+    attrs = dict(cls.__dict__)
+    attrs.update(kwargs)
+    return DocumentMetaclass.__new__(mcs=DocumentMetaclass,
+                                     name='%s%s' % (cls.__name__, uuid.uuid1()),
+                                     bases=(base_cls,), attrs=attrs)
 
 
 class DocumentMixin:
     _restruct_ = False
+    base_ignore_fields = []
+
+    @classmethod
+    def copy_base_class(cls, base_cls=None, **kwargs):
+        return copy_cls(cls, base_cls, **kwargs)
 
     @property
     def db_dict(self):
@@ -56,7 +103,7 @@ class DocumentMixin:
 
     @classmethod
     def create_with(cls, data_dict, ignore_fields=None):
-        ignore_fields = ignore_fields or CURRENT_REST_PLUS_CONFIG.config.base_ignore_fields
+        ignore_fields = ignore_fields or cls.base_ignore_fields
         s = cls()
         data_dict = {i: data_dict[i] for i in data_dict if i not in ignore_fields}
         cls.update_document(s, data_dict)
@@ -69,7 +116,7 @@ class DocumentMixin:
         :param ignore_fields:
         :return:
         """
-        ignore_fields = ignore_fields or CURRENT_REST_PLUS_CONFIG.config.base_ignore_fields
+        ignore_fields = ignore_fields or self.base_ignore_fields
         data_dict = {i: data_dict[i] for i in data_dict if i not in ignore_fields}
         DocumentMixin.update_document(self, data_dict)
 
