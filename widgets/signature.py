67d37e3703f1b7d17220ef69a06aca918d7bf57e
黄炎 <yan.huang@skyviewfund.com>
2021-06-15 18:27:27 +0800

    signature

--- widgets/signature.py
+++ widgets/signature.py
@@ -5,7 +5,7 @@ from flask import request
 from werkzeug.exceptions import Unauthorized
 
 
-class Signature(object):
+class Signature:
     """
         参考腾讯云接口鉴权 https://cloud.tencent.com/document/product/214/1526
         t = time.time()
@@ -13,65 +13,74 @@ class Signature(object):
         signature = Signature.gen_signature(action, secret_id, t)
         Signature.check_right(action, secret_id, t, signature)
     """
+    identify_keys = ['secret_id', 'timestamp', 'action']
 
-    @staticmethod
-    def get_key(key):
-        from ..config import CURRENT_REST_PLUS_CONFIG
-        return dict(CURRENT_REST_PLUS_CONFIG.config.signature_group)[key]
+    @classmethod
+    def get_key(cls, key):
+        return dict(cls.items)[key]
 
-    @staticmethod
-    def gen_signature(action, secret_id, timestamp):
+    @classmethod
+    def gen_signature(cls, action, secret_id, timestamp):
         identify = [
-            ('action', action),
-            ('secret_id', secret_id),
-            ('timestamp', timestamp),
+            (cls.identify_keys[2], action),
+            (cls.identify_keys[0], secret_id),
+            (cls.identify_keys[1], timestamp),
         ]
         identify_str = "&".join(["%s=%s" % (k, v) for k, v in identify])
-        secret_key = Signature.get_key(secret_id)
+        secret_key = cls.get_key(secret_id)
         return hmac.new(secret_key, identify_str.encode('utf-8'), hashlib.sha1).hexdigest()
 
-    @staticmethod
-    def check_right(action, secret_id, timestamp, signature):
-        cal_signature = Signature.gen_signature(action, secret_id, timestamp)
+    @classmethod
+    def check_right(cls, action, secret_id, timestamp, signature):
+        cal_signature = cls.gen_signature(action, secret_id, timestamp)
         return cal_signature == signature
 
+    @classmethod
+    def get_program_authentication_required(signature_cls):
+        def program_authentication_required(func):
+            """基于以上鉴权的装饰器方法"""
 
-def program_authentication_required(func):
-    """基于以上鉴权的装饰器方法"""
+            @wraps(func)
+            def decorated_view(*args, **kwargs):
+                identify = request.environ.get('HTTP_IDENTIFY', '')
+                try:
+                    dic = dict([i.split('=') for i in identify.split('&')])
+                    if not signature_cls.check_right(**dic):
+                        raise Unauthorized(description='signature failed:' + str(dic))
+                except Exception as e:
+                    raise Unauthorized(description='signature failed:' + str(e))
+                return func(*args, **kwargs)
 
-    @wraps(func)
-    def decorated_view(*args, **kwargs):
-        identify = request.environ.get('HTTP_IDENTIFY', '')
-        try:
-            dic = dict([i.split('=') for i in identify.split('&')])
-            if not Signature.check_right(**dic):
-                raise Unauthorized(description='signature failed:' + str(dic))
-        except Exception as e:
-            raise Unauthorized(description='signature failed:' + str(e))
-        return func(*args, **kwargs)
+            return decorated_view
+        return program_authentication_required
 
-    return decorated_view
+    @classmethod
+    def get_signature_required(signature_cls):
+        def signature_required(*params):
+            def wrapper(func):
+                project, action = params
+
+                @wraps(func)
+                def decorated_view(*args, **kwargs):
+                    identify = request.environ.get('HTTP_IDENTIFY', '')
+                    try:
+                        dic = dict([i.split('=') for i in identify.split('&')])
+                        if project != dic.get('secret_id'):
+                            raise Unauthorized(description='project error:' + str(dic))
+                        if action != dic.get('action'):
+                            raise Unauthorized(description='action error:' + str(dic))
+                        if not signature_cls.check_right(**dic):
+                            raise Unauthorized(description='signature failed:' + str(dic))
+                    except Exception as e:
+                        raise Unauthorized(description='signature failed:' + str(e))
+                    return func(*args, **kwargs)
+
+                return decorated_view
+
+            return wrapper
+
+        return signature_required
 
 
-def signature_required(*params):
-    def wrapper(func):
-        project, action = params
-
-        @wraps(func)
-        def decorated_view(*args, **kwargs):
-            identify = request.environ.get('HTTP_IDENTIFY', '')
-            try:
-                dic = dict([i.split('=') for i in identify.split('&')])
-                if project != dic.get('secret_id'):
-                    raise Unauthorized(description='project error:' + str(dic))
-                if action != dic.get('action'):
-                    raise Unauthorized(description='action error:' + str(dic))
-                if not Signature.check_right(**dic):
-                    raise Unauthorized(description='signature failed:' + str(dic))
-            except Exception as e:
-                raise Unauthorized(description='signature failed:' + str(e))
-            return func(*args, **kwargs)
-
-        return decorated_view
-
-    return wrapper
+program_authentication_required = Signature.get_program_authentication_required()
+signature_required = Signature.get_signature_required()
